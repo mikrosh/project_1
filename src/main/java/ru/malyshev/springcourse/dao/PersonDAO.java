99b37e390c2858179d0c59beb16b68494b69ea2d
mikrosh <denismalyshev777@gmail.com>
2023-05-06 23:20:27 +0300

    Lesson-27 Реализация JDBS-Template + Пишем свой собственный RowMaper

--- src/main/java/ru/malyshev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/malyshev/springcourse/dao/PersonDAO.java
@@ -1,5 +1,7 @@
 package ru.malyshev.springcourse.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.malyshev.springcourse.models.Person;
 
@@ -10,84 +12,25 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
+    private final JdbcTemplate jdbcTemplate;
 
-    //Переменные для подключения к базе данных, вообще должны быть в properties,
-    // помещать их в статические поля плохо плохо плохо, никогда не нужно так делать!!!
-    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
-    private static final String userName = "postgres";
-    private static final String password = "admin";
-
-    //Объект подключения к ДБ
-    private static Connection connection;
-
-    //Инициализиция объекта подключения
-    static {
-        try {
-            //Подгружаем класс с jdbc драйверов (в последних версиях происходит автомитаически)
-            Class.forName("org.postgresql.Driver"); //Эта хрень называется рефлексия
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, userName, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    //Образец создания подключения к базе без JDBC template описан в ветке предыдущего урока!
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    //Описание метода без JDBC template описан в ветке предыдущего урока!
     public List<Person> index(){
-        List<Person> people = new ArrayList<>();
-
-        //объект, который содержит в себе sql запрос к базе данных
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM Person";
-            //Выполняем sql запрос (statement.executeQuery(SQL))
-            // ResultSet это то, что возвращает метод выполняющий запрос,
-            // все вернувшиеся строки из таблицы, теперь лежат в нем
-            ResultSet resultSet = statement.executeQuery(SQL); //Этот метод получает данные
-
-            //Каждую строку их БД теперь надо превести в java-объект
-            //Идем по резалт сет сдвигаемся на каждом шаге на строку вперед(next)
-            while (resultSet.next()){
-                //Создаем пустышку Персон в нее будем поочереди класть объекты которые у нас в табличке лежат
-                Person person = new Person();
-                person.setId(resultSet.getInt("id")); //Значение колонки для строки на которой мы находимся (цикл же)
-                person.setName(resultSet.getString("name"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-            }
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-        return people;
+        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
     }
 
     public Person show(int id){
-        Person person = null;
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM Person WHERE id=?");
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            resultSet.next();
-            person = new Person();
-            person.setId(resultSet.getInt("id"));
-            person.setAge(resultSet.getInt("age"));
-            person.setName(resultSet.getString("name"));
-            person.setEmail(resultSet.getString("email"));
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-
-        return person;
+        //Лекция 27 (12:50) -> Объяснение эого метода и как Он работает.
+        return jdbcTemplate.query("SELECT *  FROM Person WHERE id=?",
+                        new Object[]{id},
+                        new PersonMapper())
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person){
