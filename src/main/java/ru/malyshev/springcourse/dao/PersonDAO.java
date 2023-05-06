bf036e33f773e9e6dcc85e69cef5f59520e74df0
mikrosh <denismalyshev777@gmail.com>
2023-05-07 00:29:05 +0300

    Lesson-27-part-2 Универсальный маппер и условия его реализации

--- src/main/java/ru/malyshev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/malyshev/springcourse/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package ru.malyshev.springcourse.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.malyshev.springcourse.models.Person;
@@ -22,60 +23,33 @@ public class PersonDAO {
 
     //Описание метода без JDBC template описан в ветке предыдущего урока!
     public List<Person> index(){
-        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id){
         //Лекция 27 (12:50) -> Объяснение эого метода и как Он работает.
+        //Для чтения из таблицы используем метод query() на jdbcTemplate
         return jdbcTemplate.query("SELECT *  FROM Person WHERE id=?",
                         new Object[]{id},
-                        new PersonMapper())
+                        new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(Person person){
-        try{
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO Person VALUES(1, ?, ?, ?)");
-
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2, person.getAge());
-            preparedStatement.setString(3, person.getEmail());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+        //Для обновления таблицы используем метод update() на jdbcTemplate
+        jdbcTemplate.update("INSERT INTO Person VALUES (1, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatedPerson){
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
-
-            preparedStatement.setString(1, updatedPerson.getName());
-            preparedStatement.setInt(2, updatedPerson.getAge());
-            preparedStatement.setString(3, updatedPerson.getEmail());
-            preparedStatement.setInt(4, id);
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?",
+                updatedPerson.getName(),
+                updatedPerson.getAge(),
+                updatedPerson.getEmail(),
+                id);
     }
 
     public void delete(int id){
-        try{
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM Person WHERE id=?");
-
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
 }
